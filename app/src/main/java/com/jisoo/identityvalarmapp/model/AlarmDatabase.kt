7592ff9280af421ef9ee2b237d8f478914da9c21
JISOO0817 <dkadnsk@naver.com>
2022-08-07 03:43:45 +0900

    - change database column data value

--- app/src/main/java/com/jisoo/identityvalarmapp/model/AlarmDatabase.kt
+++ app/src/main/java/com/jisoo/identityvalarmapp/model/AlarmDatabase.kt
@@ -42,6 +42,56 @@ abstract class AlarmDatabase : RoomDatabase() {
                 database.execSQL("CREATE TABLE IF NOT EXISTS CharacInfo(uid INTEGER PRIMARY KEY NOT NULL, category INTEGER NOT NULL, img INTEGER NOT NULL, job INTEGER NOT NULL, birth TEXT NOT NULL)")
                 database.execSQL("INSERT INTO CharacInfo SELECT * FROM CharacInfo_0")
                 database.execSQL("DROP TABLE IF EXISTS CharacInfo_0")
+
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Enchantress_txt WHERE uid = '1, '")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Lawyer_txt WHERE uid = '2'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.MindsEye_txt WHERE uid = '3'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Priestess_txt WHERE uid = '4'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.GraveKeeper_txt WHERE uid = '5'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Barmaid_txt WHERE uid = '6'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Explorer_txt WHERE uid = '7'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Doctor_txt WHERE uid = '8'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Prospector_txt WHERE uid = '9'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Coordinator_txt WHERE uid = '10'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Wildling_txt WHERE uid = '11'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Thief_txt WHERE uid = '12'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Embalmer_txt WHERE uid = '13'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Forward_txt WHERE uid = '14'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Acrobat_txt WHERE uid = '15'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.FemaleDancer_txt WHERE uid = '16'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Magician_txt WHERE uid = '17'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Prisoner_txt WHERE uid = '18'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Mercenary_txt WHERE uid = '19'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Perfumer_txt WHERE uid = '20'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Mechanic_txt WHERE uid = '21'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.FirstOfficer_txt WHERE uid = '22'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Seer_txt WHERE uid = '23'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.LuckyGuy_txt WHERE uid = '24'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Gardener_txt WHERE uid = '25'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Entomologist_txt WHERE uid = '26'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Postman_txt WHERE uid = '27'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Cowboy_txt WHERE uid = '28'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.SoulWeaver_txt WHERE uid = '100'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Feaster_txt WHERE uid = '101'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Geisha_txt WHERE uid = '102'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Photographer_txt WHERE uid = '103'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.AxeBoy_txt WHERE uid = '104'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.GraveKeeper_txt WHERE uid = '105'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.WuChang_txt WHERE uid = '106'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.SmileyFace_txt WHERE uid = '107'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Ripper_txt WHERE uid = '108'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Guard26_txt WHERE uid = '109'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Disciple_txt WHERE uid = '110'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.DreamWitch_txt WHERE uid = '111'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Violinist_txt WHERE uid = '112'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.BloodyQueen_txt WHERE uid = '113'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.EvilReptilian_txt WHERE uid = '114'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.Sculptor_txt WHERE uid = '115'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.MadEyes_txt WHERE uid = '116'")
+                database.execSQL("UPDATE CharacInfo SET job = R.string.HellEmber_txt WHERE uid = '117'")
+
+
+
             }
         }
 
@@ -135,102 +185,3 @@ abstract class AlarmDatabase : RoomDatabase() {
     }
 }
 
-//private val CHARACTER_DATA_en = arrayListOf(
-//    CharacInfo(1, 0, R.drawable.enchantress, "Enchantress", "0103"),
-//    CharacInfo(2, 0, R.drawable.lawyer, "Lawyer", "0112"),
-//    CharacInfo(3, 0, R.drawable.eye, "Mind's Eye", "0113"),
-//    CharacInfo(4, 0, R.drawable.priestess, "Priestess", "0201"),
-//    CharacInfo(5, 0, R.drawable.gravekeeper, "Grave Keeper", "0213"),
-//    CharacInfo(6, 0, R.drawable.barmaid, "Barmaid", "0214"),
-//    CharacInfo(7, 0, R.drawable.explorer, "Explorer", "0301"),
-//    CharacInfo(8, 0, R.drawable.doctor, "Doctor", "0317"),
-//    CharacInfo(9, 0, R.drawable.prospector, "Prospector", "0319"),
-//    CharacInfo(10, 0, R.drawable.coordinator, "Coordinator", "0403"),
-//    CharacInfo(11, 0, R.drawable.wildling, "Wildling", "0430"),
-//    CharacInfo(12, 0, R.drawable.thief, "Thief", "0507"),
-//    CharacInfo(13, 0, R.drawable.embalmer, "Embalmer", "0511"),
-//    CharacInfo(14, 0, R.drawable.forward, "Forward", "0515"),
-//    CharacInfo(15, 0, R.drawable.acrobat, "Acrobat", "0601"),
-//    CharacInfo(16, 0, R.drawable.dancer, "Female Dancer", "0609"),
-//    CharacInfo(17, 0, R.drawable.magician, "Magician", "0704"),
-//    CharacInfo(18, 0, R.drawable.prisoner, "'Prisoner'", "0710"),
-//    CharacInfo(19, 0, R.drawable.mercenary, "Mercenary", "0723"),
-//    CharacInfo(20, 0, R.drawable.perfumer, "Perfumer", "0825"),
-//    CharacInfo(21, 0, R.drawable.mechanic, "Mechanic", "0913"),
-//    CharacInfo(22, 0, R.drawable.first_officer, "First Officer", "0924"),
-//    CharacInfo(23, 0, R.drawable.seer, "Seer", "1031"),
-//    CharacInfo(24, 0, R.drawable.lucky_guy, "Lucky Guy", "1122"),
-//    CharacInfo(25, 0, R.drawable.gardner, "Gardener", "1221"),
-//    CharacInfo(26, 0, R.drawable.entomologis, "Entomologist", "1222"),
-//    CharacInfo(27, 0, R.drawable.postman, "Postman", "1225"),
-//    CharacInfo(28, 0, R.drawable.cowboy, "Cowboy", "1227"),
-//
-//    CharacInfo(100, 1, R.drawable.spider, "Soul Weaver", "0102"),
-//    CharacInfo(101, 1, R.drawable.haster, "Feaster", "0124"),
-//    CharacInfo(102, 1, R.drawable.michiko, "Geisha", "0218"),
-//    CharacInfo(103, 1, R.drawable.photo, "Photographer", "0311"),
-//    CharacInfo(104, 1, R.drawable.ulbo, "Axe Boy", "0425"),
-//    CharacInfo(105, 1, R.drawable.noru, "Gamekeeper", "0521"),
-//    CharacInfo(106, 1, R.drawable.umbrella, "Wu Chang", "0715"),
-//    CharacInfo(107, 1, R.drawable.rhkdeo, "Smiley Face", "0804"),
-//    CharacInfo(108, 1, R.drawable.ripper, "Ripper", "0807"),
-//    CharacInfo(109, 1, R.drawable.bongbog, "Guard 26", "0808"),
-//    CharacInfo(110, 1, R.drawable.sado, "'Disciple'", "0811"),
-//    CharacInfo(111, 1, R.drawable.kkumma, "Dream Witch", "1002"),
-//    CharacInfo(112, 1, R.drawable.byeall, "Violinist", "1027"),
-//    CharacInfo(113, 1, R.drawable.mari, "Bloody Queen", "1102"),
-//    CharacInfo(114, 1, R.drawable.domabaem, "Evil Reptilian", "1113"),
-//    CharacInfo(115, 1, R.drawable.jogak, "Sculptor", "1117"),
-//    CharacInfo(116, 1, R.drawable.halbae, "Mad Eyes", "1127"),
-//    CharacInfo(117, 1, R.drawable.gong, "Hell Ember", "1221")
-//)
-//
-//private val CHARACTER_DATA_ja = arrayListOf(
-//    CharacInfo(1, 0, R.drawable.enchantress, "呪術師", "0103"),
-//    CharacInfo(2, 0, R.drawable.lawyer, "弁護士", "0112"),
-//    CharacInfo(3, 0, R.drawable.eye, "心眼", "0113"),
-//    CharacInfo(4, 0, R.drawable.priestess, "祭司", "0201"),
-//    CharacInfo(5, 0, R.drawable.gravekeeper, "墓守", "0213"),
-//    CharacInfo(6, 0, R.drawable.barmaid, "バーメイド", "0214"),
-//    CharacInfo(7, 0, R.drawable.explorer, "冒険家", "0301"),
-//    CharacInfo(8, 0, R.drawable.doctor, "医師", "0317"),
-//    CharacInfo(9, 0, R.drawable.prospector, "探鉱者", "0319"),
-//    CharacInfo(10, 0, R.drawable.coordinator, "空軍", "0403"),
-//    CharacInfo(11, 0, R.drawable.wildling, "野人", "0430"),
-//    CharacInfo(12, 0, R.drawable.thief, "泥棒", "0507"),
-//    CharacInfo(13, 0, R.drawable.embalmer, "納棺師", "0511"),
-//    CharacInfo(14, 0, R.drawable.forward, "オフェンス", "0515"),
-//    CharacInfo(15, 0, R.drawable.acrobat, "曲芸師", "0601"),
-//    CharacInfo(16, 0, R.drawable.dancer, "踊り子", "0609"),
-//    CharacInfo(17, 0, R.drawable.magician, "マジシャン", "0704"),
-//    CharacInfo(18, 0, R.drawable.prisoner, "囚人", "0710"),
-//    CharacInfo(19, 0, R.drawable.mercenary, "傭兵", "0723"),
-//    CharacInfo(20, 0, R.drawable.perfumer, "調香師", "0825"),
-//    CharacInfo(21, 0, R.drawable.mechanic, "機械技師", "0913"),
-//    CharacInfo(22, 0, R.drawable.first_officer, "一等航海士", "0924"),
-//    CharacInfo(23, 0, R.drawable.seer, "占い師", "1031"),
-//    CharacInfo(24, 0, R.drawable.lucky_guy, "幸運児", "1122"),
-//    CharacInfo(25, 0, R.drawable.gardner, "庭師", "1221"),
-//    CharacInfo(26, 0, R.drawable.entomologis, "昆虫学者", "1222"),
-//    CharacInfo(27, 0, R.drawable.postman, "ポストマン", "1225"),
-//    CharacInfo(28, 0, R.drawable.cowboy, "カウボーイ", "1227"),
-//
-//    CharacInfo(100, 1, R.drawable.spider, "結魂者", "0102"),
-//    CharacInfo(101, 1, R.drawable.haster, "黄衣の王", "0124"),
-//    CharacInfo(102, 1, R.drawable.michiko, "芸者", "0218"),
-//    CharacInfo(103, 1, R.drawable.photo, "写真家", "0311"),
-//    CharacInfo(104, 1, R.drawable.ulbo, "泣き虫", "0425"),
-//    CharacInfo(105, 1, R.drawable.noru, "断罪狩人", "0521"),
-//    CharacInfo(106, 1, R.drawable.umbrella, "白黒無常", "0715"),
-//    CharacInfo(107, 1, R.drawable.rhkdeo, "道化師", "0804"),
-//    CharacInfo(108, 1, R.drawable.ripper, "リッパー", "0807"),
-//    CharacInfo(109, 1, R.drawable.bongbog, "ガードNo.26", "0808"),
-//    CharacInfo(110, 1, R.drawable.sado, "'使徒'", "0811"),
-//    CharacInfo(111, 1, R.drawable.kkumma, "夢の魔女", "1002"),
-//    CharacInfo(112, 1, R.drawable.byeall, "ヴァイオリニスト", "1027"),
-//    CharacInfo(113, 1, R.drawable.mari, "血の女王", "1102"),
-//    CharacInfo(114, 1, R.drawable.domabaem, "魔トカゲ", "1113"),
-//    CharacInfo(115, 1, R.drawable.jogak, "彫刻家", "1117"),
-//    CharacInfo(116, 1, R.drawable.halbae, "狂眼", "1127"),
-//    CharacInfo(117, 1, R.drawable.gong, "復讐者", "1221")
-//)
